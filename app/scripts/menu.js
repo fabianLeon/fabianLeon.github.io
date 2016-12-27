857c451609031636f900dea9201178cc6c4ccaf6
fabianLeon <fabianLeon@localhost.localdomain>
2016-12-27 01:10:47 -0500

    solucion errores

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -55,7 +55,7 @@ angular.module('prototipoApp')
 
     $scope.token = null;
     console.log("id_token: " + $scope.local.id_token);
-    if ($scope.local.id_token != "undefined") {
+    if ($scope.local.id_token !== null) {
       var headerObj = KJUR.jws.JWS.readSafeJSONString(b64utoutf8($scope.local.id_token.split(".")[0]));
       $scope.token = KJUR.jws.JWS.readSafeJSONString(b64utoutf8($scope.local.id_token.split(".")[1]));
       console.log(headerObj);
