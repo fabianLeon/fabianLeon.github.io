66574ea0e8ede3a436f4f16960d2316ed6bad5f0
fabianLeon <fabianLeon@localhost.localdomain>
2016-12-29 00:26:58 -0500

    Informacion adicional + uso

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -10,7 +10,7 @@ console.log(params);
 // And send the token over to the server
 var req = new XMLHttpRequest();
 // consider using POST so query isn't logged
-/*req.open('GET', 'https://' + window.location.host + '/catchtoken?' + queryString, true);
+req.open('GET', 'https://' + window.location.host + '?' + queryString, true);
 
 req.onreadystatechange = function(e) {
   if (req.readyState == 4) {
@@ -34,10 +34,11 @@ angular.module('prototipoApp')
     console.log($scope.local);
     var ctrl = this;
     ctrl.actual = $location.path();
+    console.log($location.absUrl());
     //Configuracion de parametros identificacion unica
     /*$scope.AUTORIZATION_URL = "https://wso2.intranetoas.udistrital.edu.co:9443/oauth2/authorize";
     $scope.CLIENTE_ID       = "mEEMLpePonJ91jKYB_s8sbE8slQa";
-    $scope.REDIRECT_URL     = "http://10.20.0.254/prototipo/app";
+    $scope.REDIRECT_URL     = "http://10.20.2.52/prototipo/app";
     $scope.RESPONSE_TYPE    = "code";
     $scope.SCOPE            = "openid";
 */
@@ -55,7 +56,7 @@ angular.module('prototipoApp')
     };
 
     $scope.token = null;
-    
+
     if (typeof $scope.local.id_token === 'undefined' || $scope.local.id_token === null ) {
       console.log("hola");
     } else {
