0ae39b5970fe6ff63ed099a737499e5551f8cef1
fabianLeon <fabianLeon@localhost.localdomain>
2016-12-29 08:09:09 -0500

    controller de footer

--- app/scripts/controllers/footer.js
+++ app/scripts/controllers/footer.js
@@ -1,5 +1,33 @@
 
 angular.module('prototipoApp')
   .controller('footerCtrl', function($scope) {
-
+     var ctrl = this;
+     ctrl.enlaces_universitarios = [{nombre: 'Transparencia',
+     link: '#/'},
+     {nombre: 'Normatividad',
+     link: '#/'},
+     {nombre: 'Trámites',
+     link: '#/'},
+     {nombre: 'General',
+     link: '#/'},
+     {nombre: 'Docente',
+     link: '#/'},
+     {nombre: 'Académica Estudiantil ',
+     link: '#/'},
+     {nombre: 'Derechos Pecuniarios',
+     link: '#/'},
+     {nombre: 'Sistema de Notificaciones',
+     link: '#/'},
+     {nombre: 'CSU',
+     link: '#/'},
+     {nombre: 'PIGA',
+     link: '#/'},
+     {nombre: 'Bitácora',
+     link: '#/'},
+     {nombre: 'Noticias anterioresg',
+     link: '#/'},
+     {nombre: 'Área de Red UDNet',
+     link: '#/'},
+     {nombre: 'Administración PWI',
+     link: '#/'}]
   });
