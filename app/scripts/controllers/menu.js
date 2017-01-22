519b81beae0f0c3e2f2ea5ec2c63bac16e00406e
fabianLeon <fabianLeon@localhost.localdomain>
2017-01-22 01:02:28 -0500

    menu ejemplo + miga e pan anterior

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -5,7 +5,54 @@ angular.module('prototipoApp')
     ctrl.actual = $location.path();
     $scope.token_service = token_service;
 
-
+    $scope.menu_service = [
+    {
+      "Id": 4,
+      "Nombre": "about",
+      "Url": "about",
+      "Opciones": null
+    },
+    {
+      "Id": 2,
+      "Nombre": "nivel 1",
+      "Url": "argo.com/gestioncontratos",
+      "Opciones": [
+        {
+          "Id": 3,
+          "Nombre": "nivel 2",
+          "Url": "contrato_horaCatedra",
+          "Opciones": [
+            {
+              "Id": 7,
+              "Nombre": "nivel 3",
+              "Url": "agregar_contrato",
+              "Opciones": [
+                {
+                  "Id": 8,
+                  "Nombre": "nivel 4",
+                  "Url": "agregar_contrato",
+                  "Opciones": null
+                }
+              ]
+            },
+            {
+              "Id": 7,
+              "Nombre": "Agregar Contrato",
+              "Url": "agregar_contrato",
+              "Opciones": null
+            }
+          ]
+        },
+        {
+          "Id": 4,
+          "Nombre": "Contratos asdf",
+          "Url": "contrato_admin",
+          "Opciones": null
+        }
+      ]
+    }
+  ];
+  console.log($scope.menu_service[1].Opciones.length);
     //Pendiente por definir json del menu
     (function($) {
       $(document).ready(function() {
