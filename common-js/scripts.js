0c412b364b1e0f8427e5ed82253d77015e75da8f
fabianleon <fadarsaleeing@gmail.com>
2019-06-10 17:09:22 -0500

    error latitud

--- common-js/scripts.js
+++ common-js/scripts.js
@@ -1,56 +1,56 @@
 
-	
+
 (function ($) {
 
-    "use strict";
+	"use strict";
 
 	// JQUERY LIGHT BOX
-	
-	if ( $.isFunction($.fn.fluidbox) ) {
+
+	if ($.isFunction($.fn.fluidbox)) {
 		$('a').fluidbox();
 	}
 
-	
-	$('a[href="#"]').on('click', function(event){
+
+	$('a[href="#"]').on('click', function (event) {
 		return;
 	});
-	
+
 	// COUNTDOWN TIME 
-	
+
 	countdownTime();
-	
-	
-	$('[data-nav-menu]').on('click', function(event){
-		
+
+
+	$('[data-nav-menu]').on('click', function (event) {
+
 		var $this = $(this),
 			visibleHeadArea = $this.data('nav-menu');
-		
+
 		$(visibleHeadArea).toggleClass('visible');
-		
+
 	});
-	
-	
+
+
 	var winWidth = $(window).width();
 	dropdownMenu(winWidth);
-	
-	$(window).on('resize', function(){
+
+	$(window).on('resize', function () {
 		dropdownMenu(winWidth);
-		
+
 	});
-	
+
 	// Circular Progress Bar
-	
+
 	var isAnimated = false;
-	
-	
+
+
 })(jQuery);
 
 
 
-function countdownTime(){
-	
-	if(isExists('#clock')){
-		$('#clock').countdown('2019/09/13', function(event){
+function countdownTime() {
+
+	if (isExists('#clock')) {
+		$('#clock').countdown('2019/09/13', function (event) {
 			var $this = $(this).html(event.strftime(''
 				+ '<div class="time-sec"><span class="title">%D</span> días </div>'
 				+ '<div class="time-sec"><span class="title">%H</span> horas </div>'
@@ -59,38 +59,38 @@ function countdownTime(){
 		});
 	}
 }
-function dropdownMenu(winWidth){
-	
-	if(winWidth > 767){
-		
-		$('.main-menu li.drop-down').on('mouseover', function(){
+function dropdownMenu(winWidth) {
+
+	if (winWidth > 767) {
+
+		$('.main-menu li.drop-down').on('mouseover', function () {
 			var $this = $(this),
 				menuAnchor = $this.children('a');
-				
+
 			menuAnchor.addClass('mouseover');
-			
-		}).on('mouseleave', function(){
+
+		}).on('mouseleave', function () {
 			var $this = $(this),
 				menuAnchor = $this.children('a');
-				
+
 			menuAnchor.removeClass('mouseover');
 		});
-		
-	}else{
-		
-		$('.main-menu li.drop-down > a').on('click', function(){
-			
-			if($(this).attr('href') == '#') return false;
-			if($(this).hasClass('mouseover')){ $(this).removeClass('mouseover'); }
-			else{ $(this).addClass('mouseover'); }
+
+	} else {
+
+		$('.main-menu li.drop-down > a').on('click', function () {
+
+			if ($(this).attr('href') == '#') return false;
+			if ($(this).hasClass('mouseover')) { $(this).removeClass('mouseover'); }
+			else { $(this).addClass('mouseover'); }
 			return false;
 		});
 	}
-	
+
 }
 
-function isExists(elem){
-	if ($(elem).length > 0) { 
+function isExists(elem) {
+	if ($(elem).length > 0) {
 		return true;
 	}
 	return false;
@@ -101,142 +101,142 @@ function initMap() {
 	// Create a new StyledMapType object, passing it an array of styles,
 	// and the name to be displayed on the map type control.
 	var styledMapType = new google.maps.StyledMapType(
-	[
-		{
-			"featureType": "administrative",
-			"elementType": "all",
-			"stylers": [
-				{
-					"saturation": "-100"
-				}
-			]
-		},
-		{
-			"featureType": "administrative.province",
-			"elementType": "all",
-			"stylers": [
-				{
-					"visibility": "off"
-				}
-			]
-		},
-		{
-			"featureType": "landscape",
-			"elementType": "all",
-			"stylers": [
-				{
-					"saturation": -100
-				},
-				{
-					"lightness": 65
-				},
-				{
-					"visibility": "on"
-				}
-			]
-		},
-		{
-			"featureType": "poi",
-			"elementType": "all",
-			"stylers": [
-				{
-					"saturation": -100
-				},
-				{
-					"lightness": "50"
-				},
-				{
-					"visibility": "simplified"
-				}
-			]
-		},
-		{
-			"featureType": "road",
-			"elementType": "all",
-			"stylers": [
-				{
-					"saturation": "-100"
-				}
-			]
-		},
-		{
-			"featureType": "road.highway",
-			"elementType": "all",
-			"stylers": [
-				{
-					"visibility": "simplified"
-				}
-			]
-		},
-		{
-			"featureType": "road.arterial",
-			"elementType": "all",
-			"stylers": [
-				{
-					"lightness": "30"
-				}
-			]
-		},
-		{
-			"featureType": "road.local",
-			"elementType": "all",
-			"stylers": [
-				{
-					"lightness": "40"
-				}
-			]
-		},
-		{
-			"featureType": "transit",
-			"elementType": "all",
-			"stylers": [
-				{
-					"saturation": -100
-				},
-				{
-					"visibility": "simplified"
-				}
-			]
-		},
-		{
-			"featureType": "water",
-			"elementType": "geometry",
-			"stylers": [
-				{
-					"hue": "#ffff00"
-				},
-				{
-					"lightness": -25
-				},
-				{
-					"saturation": -97
-				}
-			]
-		},
-		{
-			"featureType": "water",
-			"elementType": "labels",
-			"stylers": [
-				{
-					"lightness": -25
-				},
-				{
-					"saturation": -100
-				}
-			]
-		}
-	],
-		{name: 'Styled Map'});
+		[
+			{
+				"featureType": "administrative",
+				"elementType": "all",
+				"stylers": [
+					{
+						"saturation": "-100"
+					}
+				]
+			},
+			{
+				"featureType": "administrative.province",
+				"elementType": "all",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"featureType": "landscape",
+				"elementType": "all",
+				"stylers": [
+					{
+						"saturation": -100
+					},
+					{
+						"lightness": 65
+					},
+					{
+						"visibility": "on"
+					}
+				]
+			},
+			{
+				"featureType": "poi",
+				"elementType": "all",
+				"stylers": [
+					{
+						"saturation": -100
+					},
+					{
+						"lightness": "50"
+					},
+					{
+						"visibility": "simplified"
+					}
+				]
+			},
+			{
+				"featureType": "road",
+				"elementType": "all",
+				"stylers": [
+					{
+						"saturation": "-100"
+					}
+				]
+			},
+			{
+				"featureType": "road.highway",
+				"elementType": "all",
+				"stylers": [
+					{
+						"visibility": "simplified"
+					}
+				]
+			},
+			{
+				"featureType": "road.arterial",
+				"elementType": "all",
+				"stylers": [
+					{
+						"lightness": "30"
+					}
+				]
+			},
+			{
+				"featureType": "road.local",
+				"elementType": "all",
+				"stylers": [
+					{
+						"lightness": "40"
+					}
+				]
+			},
+			{
+				"featureType": "transit",
+				"elementType": "all",
+				"stylers": [
+					{
+						"saturation": -100
+					},
+					{
+						"visibility": "simplified"
+					}
+				]
+			},
+			{
+				"featureType": "water",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"hue": "#ffff00"
+					},
+					{
+						"lightness": -25
+					},
+					{
+						"saturation": -97
+					}
+				]
+			},
+			{
+				"featureType": "water",
+				"elementType": "labels",
+				"stylers": [
+					{
+						"lightness": -25
+					},
+					{
+						"saturation": -100
+					}
+				]
+			}
+		],
+		{ name: 'Styled Map' });
 
 	// Create a map object, and include the MapTypeId to add
 	// to the map type control.
 
-	var uluru = {lat: 4.826857, lng: -74.033154,15};
-    var map = new google.maps.Map(document.getElementById('map'), {
+	var uluru = { lat: 4.826857, lng: -74.033154 };
+	var map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 4,
 		center: uluru
-    });
-		
+	});
+
 	var image = 'images/google-marker.png';
 	var marker = new google.maps.Marker({
 		position: uluru,
